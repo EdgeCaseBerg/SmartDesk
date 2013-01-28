d3f4f240ad41ae5096caa6891e0c5161a04341cf
EJEHardenBerg <ejayeldridge@gmail.com>
2013-01-27 23:23:00 -0500

    Updated the plan and added some skeleton code

---
+++ C/engine.h
@@ -0,0 +1,12 @@
+#ifndef __ENGINE_H__
+	#define __ENGINE_H__
+#endif
+
+//Run the basic network accept loop and update the structure holding information
+void processNewData();
+//Update the graphics to represent the new information
+void updateGraphics();
+//check for mouse clicks and handle it
+void processUserInput();
+//Update if transmitting to another desk
+void updateRemote();
