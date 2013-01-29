1877e72d2da427773ffd281a918dde73ea925c04
EJEHardenBerg <ejayeldridge@gmail.com>
2013-01-29 00:00:16 -0500

    changed header for engine not to have skeleton code and just have the functions I've made so far

--- C/engine.h
+++ C/engine.h
@@ -1,15 +1,8 @@
 #ifndef __ENGINE_H__
 	#define __ENGINE_H__
 
-
-//Run the basic network accept loop and update the structure holding information
-void processNewData();
-//Update the graphics to represent the new information
-void updateGraphics();
-//check for mouse clicks and handle it
-void processUserInput();
-//Update if transmitting to another desk
-void updateRemote();
-
+//Creates a memory mapped file using the constants defined in conf.h
+//Returns -1 on failure, file descriptor for file on success.
+int createMemShare();
 
 #endif
